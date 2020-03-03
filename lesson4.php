4334113895e24d66edcf7b1538528293fc3018b8
Karyakin <viktor_karyakin@mail.ru>
2020-03-03 12:52:39 +0400

    Not JS

--- lesson4.php
+++ lesson4.php
@@ -3,18 +3,14 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>Лекция 4 - домашнее задание</title>
-	<script src="/js/jquery.js"></script><!-- Библиотека jQuery -->
-	<script src="/js/jscript.js"></script><!--свой JS-код-->
-	<script src="/js/jquery.form.js"></script>
 </head>
 <body align="center">
 	<h1>Лекция 4 - Формы и файлы</h1>
 	<h2>Форма</h2>
 	<div id="output">Выберите файлы и заполните поле:</div><br>
-	<form method="post" enctype="multipart/form-data" id="formdata">
+	<form method="post" enctype="multipart/form-data" action="active_form.php">
 		<input type="file" name="files[]" accept="text/plain" multiple> <br><br>
 		<textarea name="str" cols="100" rows="25"></textarea><br><br>
-		<input type="button" name="btn_active_form" id="btn_active_form" value="Выполнить">
+		<input type="submit" name="btn_active_form" value="Выполнить">
 	</form>
-
 </body>
